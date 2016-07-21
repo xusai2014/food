70ae55db1fdccf43719a4865b40ae2c3ac928c80
XU Sai <xusai@microduino.cc>
2016-07-21 10:13:04 +0800

    [innitail] remove setinteral ,everyone can add user info

--- client/mates/mates.js
+++ client/mates/mates.js
@@ -1,33 +1,74 @@
+
+
+Template.mates.onCreated(function(){
+  Template.mates.date = new ReactiveVar(new Date());
+  setInterval(function(){
+    Template.mates.date.set(new Date());
+  },100);
+})
 Template.mates.helpers({
+  currentDate(){
+    let date = Template.mates.date.get();
+    return "  "+date.getFullYear()+"年"+(date.getMonth()+1)+"月"+date.getDate()+"日" + date.getHours()+"时"+date.getMinutes()+"分"+"  星期  "+date.getDay();
+  },
   mates: function() {
     return Mates.find();
   },
   parts: function(){
     return [
     {
+      name:"Boss",
+
+    },
+    {
       name:"系统部",
 
     },
     {
-      name:"设计部",
+      name:"人事行政",
 
     },
     {
+      name:"销售",
+
+    },{
+      name:"社区市场",
+
+    },{
       name:"产品部",
 
     },
     {
-      name:"硬件部",
+      name:"嵌入式",
 
-    },
-    {
-      name:"临时访客",
+    },{
+      name:"分公司",
+
+    },{
+      name:"访客",
 
     }
     ]
   },
   foodNumber:function(){
     return Mates.find({food:true}).fetch().length;
+  },
+  textInput:function(food){
+     
+     if(food){
+        return '取消';
+
+     } else {
+        return '订餐';
+     }
+  },
+  classActive:function(food){
+    if(food){
+        return 'activeS';
+
+     } else {
+        return 'inactive';
+     }
   }
 });
 
@@ -35,7 +76,7 @@ Template.mates.events({
   'click .add':function(e){
     let name = $('.name').val();
     let part = $('.parts').find("option:selected").val();
-    if(name && part){
+    if(name && part !== "请选择"){
       Mates.insert({
         name:name,
         part:part,
@@ -53,12 +94,12 @@ Template.mates.events({
     let mate = Mates.findOne({_id:id});
 
      
-     if(mate.food){
-        $(e.currentTarget).html('已取消');
+     // if(mate.food){
+     //    $(e.currentTarget).html('订餐');
 
-     } else {
-        $(e.currentTarget).html('订餐成功');
-     }
+     // } else {
+     //    $(e.currentTarget).html('取消');
+     // }
      Mates.update(id,{$set:{
       food:!mate.food
      }})
--- server/main.js
+++ server/main.js
@@ -2,19 +2,19 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  setInterval(Meteor.bindEnvironment(function() {
-    let date = new Date();
-    if(date.getHours()==5){
-      let mates = Mates.find().fetch();
-      if(mates.length>0){
-        _.each(mates,function(v,i){
-          Mates.update(v._id,{$set:{
-            food:false
-           }});
-        });
-
-      }
+  // timeer = setInterval(Meteor.bindEnvironment(function() {
+  //   let date = new Date();
+  //   if(date.getHours()==14){
+  //     let mates = Mates.find().fetch();
+  //     if(mates.length>0){
+  //       _.each(mates,function(v,i){
+  //         Mates.update(v._id,{$set:{
+  //           food:false
+  //          }});
+  //       });
+  //       clearInterval(timmer);
+  //     }
       
-    }
-  }),20*60000);
+  //   }
+  // }),20*60000);
 });
